6f97bdfd723768fd8313d6400205c2dc0631cefb
SupNeit <nolanbellaire@gmail.com>
2025-10-08 16:23:46 +0200

    Cours sur le JS.

--- js/base.js
+++ js/base.js
@@ -94,9 +94,105 @@ let x = null;
 const tasse = {
     color: "Rouge",
     material: "Céramique",
-    price : 1,
-    content_volume : 25
+    price: 1,
+    content_volume: 25
 };
 console.log(tasse);
 
 
+const eleve = {
+    name: "Jean-Michel",
+    age: 18,
+    class: "Terminale",
+    notes: [13, 18, 19, 2, 16, 15.5]
+};
+console.log(eleve.name);
+// calcul de la moyenne de Jean-Michel, en codage naïf
+let moyenne = (eleve.notes[0] + eleve.notes[1] + eleve.notes[2] + eleve.notes[3] + eleve.notes[4] + eleve.notes[5]) / 6;
+console.log(moyenne);
+
+
+// OPERATEUR
+
+// l'affectation
+// on donne la valeur de 7 a la variable "aa"
+let aa = 7;
+
+
+// l'addition
+// on additionne 12 + 4 donc "aa" est égal à 16
+aa = 12 + 4;
+console.log(aa);
+// l'affectation après addition
+// on ajoute le 4 a l'ancienne valeur
+aa += 4;
+console.log(aa);
+
+// la soustraction
+// on soustracte 20 - 4 donc "bb" est égal à 16
+let bb = 20 - 4;
+console.log(bb);
+// l'affectation après soustraction
+// on retire le 4 a l'ancienne valeur
+bb -= 4;
+console.log(bb);
+
+// la multiplication
+let cc = 5 * 8;
+console.log(cc);
+// l'affectation après multiplication
+cc *= 2;
+console.log(cc);
+
+// la division
+let dd = 20 / 4;
+console.log(dd);
+// l'affectation après division
+dd /= 2;
+console.log(dd);
+
+
+
+// les modulos
+let ee = 13 % 5;
+console.log(ee);
+
+// les exponentiels
+let ff = 3 ** 2;
+console.log(ff);
+
+
+// les conditions
+
+let gg = 4;
+let hh = 4;
+let ii = 9;
+
+if (gg == hh) {
+    // si gg = hh
+} else {
+    // si gg != hh
+
+}
+
+// vérification égalité non-stricte
+if (6 == "6") {
+    console.log("ça marche");
+}
+
+// vérification égalité stricte
+// Elle est fausse car un Number ne peut pas etre égal a une Str
+if (6 === "6") {
+    /////////
+} else {
+    console.log("ça marchera pas");
+}
+
+if (eleve.age >= 18) {
+    // On fait une concaténation (coller des str entre elles)
+    // Avec le nom de l'eleve et un texte descriptif
+    console.log(eleve.name + " est majeur")
+} else {
+    // concaténation de littéral de gabarit
+    console.log(`${eleve.name} n'est pas majeur`)
+}
---
+++ js/dom.js
@@ -0,0 +1,87 @@
+const title = document.getElementById("title");
+console.log(title);
+
+
+const btn = document.getElementById("btn");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+let clickNumber = document.getElementById("click-number");
+
+
+btn.addEventListener("click", function(){
+    title.innerHTML = "Blblblblblblbl"
+})
+
+
+//Evenement avec fonction fléchée à éviter 
+
+// btn.addEventListener("click", () => {
+//     title.innerHTML = "Blblblblblblbl"
+// })
+
+let click = 0;
+btn2.addEventListener("click", function(){
+    click++ ; // équivalent à click += 1
+    clickNumber.innerHTML = click;
+})
+
+btn3.addEventListener("click", function(){
+    click-- ; // équivalent à click += 1
+    clickNumber.innerHTML = click;
+})
+
+
+// Avec les input
+
+const inputText = document.getElementById("input-text");
+const input1 = document.getElementById("input1")
+// Permet d'effacer le contenu de l'input lorsque l'on recharge la page
+input1.value = ""
+
+
+input1.addEventListener("input", function(){
+    let valueInput = this.value;
+    inputText.innerHTML = valueInput;
+
+})
+
+
+// Hierarchie
+
+const mom = document.getElementById("mom");
+
+// ChildNodes
+// Récupérer tous les éléments de "mom" (meme le texte)
+console.log(mom.childNodes);
+// On récupère les éléments à l'intérieur de l'index 5
+console.log(mom.childNodes[5].childNodes);
+
+
+// Children
+// Récupérer tous les éléments enfants directs de "mom" (pas le texte)
+console.log(mom.children);
+
+// First Child
+console.log(mom.firstChild);
+
+// FirstElementChild
+console.log(mom.firstElementChild);
+
+
+// On va supprimer le li de mom à partir des paramètres précédents
+console.log(mom.children[2].firstElementChild.firstElementChild.remove());
+
+// Création d'un élément
+let img = document.createElement("img");
+// On modifie son src
+img.src = "https://previews.123rf.com/images/captainimages/captainimages1403/captainimages140300222/26708972-close-up-of-one-piece-of-fried-potato-chips-isolated-on-white-background.jpg";
+img.style.height = "200px";
+img.style.width = "200px";
+img.alt = "Une chips"
+
+
+// Maintenant qu l'on a crée notre image, on peut l'accrocher à un autre élément en tant qu'enfant
+mom.appendChild(img)
+
+
+
